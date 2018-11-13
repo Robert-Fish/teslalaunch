357f70da7629958885a23017ade5d4ad078e3cfb
Robert-Fish <elasys63+github@gmail.com>
2018-11-13 17:45:15 +1000

    added client

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import logoSpace from "./logo.svg";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <img
+              src={logoSpace}
+              alt=""
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
